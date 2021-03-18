ff1c1751017330e60b80f1cb51343a5f58935212
toBeOfUse <mitchjacov@gmail.com>
2021-03-18 02:13:11 -0400

    add vue-svg-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const isDev = process.env["NODE_ENV"] != "production";
 module.exports = {
-    "mode": isDev ? "development" : "production",
-    "entry": "./src/index.js",
-    "output": {
-        "filename": "bundle.js"
-    },
-    devtool: isDev ? "inline-cheap-module-source-map" : false,
-    module: {
-        rules: [{
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new VueLoaderPlugin()
-    ]
+  mode: isDev ? "development" : "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+  },
+  devtool: isDev ? "inline-cheap-module-source-map" : false,
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["vue-svg-loader"],
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
 };
