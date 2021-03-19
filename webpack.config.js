257bdee0e407c5145cb414e774dbdf3d147b72ec
toBeOfUse <mitchjacov@gmail.com>
2021-03-19 07:51:51 -0400

    upgrade webpack, install sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
         test: /\.svg$/,
         use: ["vue-svg-loader"],
       },
